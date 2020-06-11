f69db4dac48f144d5b874dacc2862ec2f0953ae2
李永飞 <liyongfei@atoshi.org>
2020-06-11 14:01:33 +0800

    Initial Commit

---
+++ YFMultiTabPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YFMultiTabPage
+//
+//  Created by liyongfi on 2020/6/11.
+//  Copyright © 2020 liyongfi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
